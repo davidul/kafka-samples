1af9e3144c794b9b3f13a788bc2a1afda4ffc1b2
DAVIDUL <ulicny.david@gmail.com>
2020-12-18 05:28:28 +0100

    CRLF -> LF

--- docker/kafka/start-kafka.sh
+++ docker/kafka/start-kafka.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
 
 $KAFKA_HOME/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic my-topic --partitions 3 --replication-factor 3 --config max.message.bytes=64000 --config flush.messages=1
--- docker/sample-producer/wait-and-start.sh
+++ docker/sample-producer/wait-and-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 set -m
 set -e
 
